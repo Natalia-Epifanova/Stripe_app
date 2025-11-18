f5d6c29455013573c9d448e9b644ec215e38721e
Natalia Epifanova <epif97@mail.ru>
2025-11-18 15:59:29 +0300

    add model Item

--- stripe_app/models.py
+++ stripe_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название элемента")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание элемента")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена элемента"
+    )
+
+    class Meta:
+        verbose_name = "Элемент"
+        verbose_name_plural = "Элементы"
