59eb79dec26c6d87a6a8887d6bede3a586618fcd
Natalia Epifanova <epif97@mail.ru>
2025-11-20 11:59:32 +0300

    preparing pythonanywhere

--- config/settings.py
+++ config/settings.py
@@ -14,9 +14,17 @@ STRIPE_SECRET_KEY_USD = os.getenv("STRIPE_SECRET_KEY_USD")
 STRIPE_PUBLIC_KEY_EUR = os.getenv("STRIPE_PUBLIC_KEY_EUR")
 STRIPE_SECRET_KEY_EUR = os.getenv("STRIPE_SECRET_KEY_EUR")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "web"]
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "web",
+    ".pythonanywhere.com",
+    "NataliaYep.pythonanywhere.com",
+]
 
 
 INSTALLED_APPS = [
@@ -31,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -58,17 +67,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
+# DATABASES ДЛЯ MYSQL
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME"),
-        "USER": os.getenv("DATABASE_USER"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DATABASE_HOST", "db"),
-        "PORT": os.getenv("DATABASE_PORT", "5432"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE_NAME"),
+        "USER": os.getenv("MYSQL_DATABASE_USER"),
+        "PASSWORD": os.getenv("MYSQL_DATABASE_PASSWORD"),
+        "HOST": os.getenv("MYSQL_DATABASE_HOST"),
+        "PORT": os.getenv("MYSQL_DATABASE_PORT"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+# DATABASES ДЛЯ POSTGRESQL
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DATABASE_NAME"),
+#         "USER": os.getenv("DATABASE_USER"),
+#         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+#         "HOST": os.getenv("DATABASE_HOST", "db"),
+#         "PORT": os.getenv("DATABASE_PORT", "5432"),
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -97,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
